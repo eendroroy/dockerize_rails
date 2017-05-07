a50c1eab604b7a20219f2ec0e4b4433cd8f73858
indrajit <eendroroy@gmail.com>
2017-05-08 00:09:08 +0600

    rename files

--- lib/rockered.rb
+++ lib/rockered.rb
@@ -1,8 +1,8 @@
-require 'rockered/rockered_command_line'
-require 'rockered/rockered_config_generator'
-require 'rockered/rockered_config_helper'
-require 'rockered/rockered_docker_helper'
-require 'rockered/rockered_paths'
+require 'rockered/command_line'
+require 'rockered/config_generator'
+require 'rockered/config_helper'
+require 'rockered/docker_helper'
+require 'rockered/paths'
 require 'rockered/version'
 require 'rockered/constants'
 
