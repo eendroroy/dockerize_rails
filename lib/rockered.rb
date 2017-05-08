edbd0616ef7364dafc3029bc707b1084736e38bf
indrajit <eendroroy@gmail.com>
2017-05-08 17:35:41 +0600

    refactoring

--- lib/rockered.rb
+++ lib/rockered.rb
@@ -1,14 +1,14 @@
-require 'rockered/rockered_config'
-require 'rockered/command_line_methods'
-require 'rockered/config_generator'
-require 'rockered/config_loader'
-require 'rockered/docker_helper'
-require 'rockered/paths'
-require 'rockered/helpers'
 require 'rockered/version'
 require 'rockered/constants'
+require 'rockered/paths'
+require 'rockered/rockered_config'
 require 'rockered/command_line'
+require 'rockered/command_line_methods'
+require 'rockered/helpers'
+require 'rockered/config_loader'
+require 'rockered/config_generator'
 require 'rockered/r_name_space'
+require 'rockered/docker_helper'
 
 module Rockered
   private_constant :CommandLineMethods
