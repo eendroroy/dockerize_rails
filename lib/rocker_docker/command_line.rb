bae7bd8e2b10a0b067f7b94bf126f8b80d2f204f
indrajit <eendroroy@gmail.com>
2017-05-09 23:23:40 +0600

    dummy gem

--- lib/rocker_docker/command_line.rb
+++ lib/rocker_docker/command_line.rb
@@ -1,6 +1,8 @@
 module RockerDocker
   module CommandLine
     def run
+      puts 'Please wait for version > 0'
+      exit(1)
       opts = Helpers.parse_opts
       exit(CommandLineMethods.invoke(opts.command, opts.args))
     end
