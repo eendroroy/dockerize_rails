22c1f5a4eb6de02f42db0725b44d767d6e8ee8bb
indrajit <eendroroy@gmail.com>
2017-05-30 11:00:46 +0600

    update tests

--- spec/dockerize_rails_spec.rb
+++ spec/dockerize_rails_spec.rb
@@ -6,22 +6,30 @@ RSpec.describe DockerizeRails do
   end
 
   it 'successfully runs help command' do
-    expect(DockerizeRailsCLI.run('help', [])).to be 0
+    expect(DockerizeRailsCLI.run('help', args: [])).to be 0
   end
 
   it 'successfully runs configure command' do
-    expect(DockerizeRailsCLI.run('configure', [])).to be 0
+    expect(DockerizeRailsCLI.run('configure', args: [])).to be 0
+  end
+
+  it 'successfully runs configure --skip-desc command' do
+    expect(DockerizeRailsCLI.run('configure', args: ['--skip-desc'])).to be 0
   end
 
   it 'successfully runs dockerize command' do
-    expect(DockerizeRailsCLI.run('dockerize', [])).to be 0
+    expect(DockerizeRailsCLI.run('dockerize', args: [])).to be 0
   end
 
   it 'successfully runs undockerize command' do
-    expect(DockerizeRailsCLI.run('undockerize', [])).to be 0
+    expect(DockerizeRailsCLI.run('undockerize', args: [])).to be 0
+  end
+
+  it 'successfully runs undockerize --purge command' do
+    expect(DockerizeRailsCLI.run('undockerize', args: ['--purge'])).to be 0
   end
 
   it 'fails to run undefined command' do
-    expect(DockerizeRailsCLI.run('undefined', [])).to be 1
+    expect(DockerizeRailsCLI.run('undefined', args: [])).to be 1
   end
 end
