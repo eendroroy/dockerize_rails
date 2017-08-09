008a946dec9a3967cb8809845e2996dcceedc2c0
indrajit <eendroroy@gmail.com>
2017-08-09 06:11:39 +0600

    fix some more rubocop issues

--- lib/dockerize_rails/docker_commands/docker_start.rb
+++ lib/dockerize_rails/docker_commands/docker_start.rb
@@ -49,26 +49,27 @@ module DockerizeRails
       end
 
       def self.build_options(service, image_name)
-        options = {}
-        options['Image'] = image_name
-        options['name'] = "#{image_name.split(':')[0]}_container"
+        options = {
+          'Image' => image_name,
+          'name' => "#{image_name.split(':')[0]}_container"
+        }
         options['ExposedPorts'] = { "#{service['expose']}/tcp" => {} } if service.key? 'expose'
         if service.key? 'ports'
           options['HostConfig'] = {}
           options['HostConfig'] = {
-              'PortBindings' => Hash[service['ports'].map do |ports|
-                ["#{ports.split(':')[0]}/tcp", [{ 'HostPort' => ports.split(':')[1] }]]
-              end]
+            'PortBindings' => Hash[service['ports'].map do |ports|
+              ["#{ports.split(':')[0]}/tcp", [{ 'HostPort' => ports.split(':')[1] }]]
+            end]
           }
         end
         if service.key? 'volumes'
           options['Volumes'] = Hash[service['volumes'].map do |volume|
             source =
-                if volume.split(':')[0].start_with?('/')
-                  volume.split(':')[0]
-                else
-                  File.join(DockerizeRails::PATHS.current, volume.split(':')[0])
-                end
+              if volume.split(':')[0].start_with?('/')
+                volume.split(':')[0]
+              else
+                File.join(DockerizeRails::PATHS.current, volume.split(':')[0])
+              end
             [source, { volume.split(':')[1] => volume.length > 2 ? volume.split(':')[2] : 'rw' }]
           end]
         end
