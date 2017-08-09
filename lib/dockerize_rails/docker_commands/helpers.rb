ded5e74bde16cdd6a09eeb366e5fa6a9380fcc79
indrajit <eendroroy@gmail.com>
2017-08-09 06:06:30 +0600

    fix some rubocop issues

--- lib/dockerize_rails/docker_commands/helpers.rb
+++ lib/dockerize_rails/docker_commands/helpers.rb
@@ -1,7 +1,7 @@
 module DockerizeRails
   module DockerCommands
     module Helpers
-      def self.get_services_from_docker_compose
+      def self.services_from_docker_compose
         docker_compose = YAML.load_file "#{DockerizeRails::PATHS.current}/docker-compose.yml"
         docker_compose['services']
       end
