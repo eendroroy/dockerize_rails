5f7140b47c13f15b632e8d041c6bfc2cc1f65595
indrajit <eendroroy@gmail.com>
2017-08-11 00:44:01 +0600

    docker_start

--- lib/dockerize_rails/docker_commands/docker_start.rb
+++ lib/dockerize_rails/docker_commands/docker_start.rb
@@ -38,32 +38,46 @@ module DockerizeRails
         1
       end
 
+      # rubocop:disable Metrics/AbcSize
       def self.docker_start(definitions, service)
         options = build_options(definitions, service)
-        puts options
         container = Docker::Container.create options
         binds =
           if definitions.key? 'volumes'
             { 'Binds' => definitions['volumes'].map do |volume|
-                [if volume.split(':')[0].start_with?('/')
-                  volume.split(':')[0]
-                else
-                  File.join(DockerizeRails::PATHS.current, volume.split(':')[0])
-                end,
-                volume.split(':')[1],
-                volume.length > 2 ? volume.split(':')[2] : 'rw'].join(':')
+              [if volume.split(':')[0].start_with?('/')
+                 volume.split(':')[0]
+               else
+                 File.join(DockerizeRails::PATHS.current, volume.split(':')[0])
+               end,
+               volume.split(':')[1],
+               volume.length > 2 ? volume.split(':')[2] : 'rw'].join(':')
             end }
           end
-        puts binds
         container.start binds
       end
+      # rubocop:enable Metrics/AbcSize
 
+      # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
       def self.build_options(definitions, service)
-        {
-          'Image' => Helpers.get_name(service, :image), 'name' => Helpers.get_name(service, :container)
-        }.merge(
-          definitions.key?('expose') ? { 'ExposedPorts' => { "#{definitions['expose']}/tcp" => {} } } : {}
-        ).merge(
+        options = Helpers.recurse_merge(
+          {
+            'Image' => Helpers.get_name(service, :image),
+            'name' => Helpers.get_name(service, :container),
+            'Hostname' => '0.0.0.0'
+          },
+          if definitions.key?('expose')
+            {
+              'ExposedPorts' => Hash[definitions['expose'].map { |ports| ["#{ports}/tcp", {}] }],
+              'HostConfig' => { 'PortBindings' => Hash[definitions['expose'].map { |ports| ["#{ports}/tcp", [{}]] }] }
+            }
+          else
+            {}
+          end
+        )
+        options = Helpers.recurse_merge(
+          options,
           if definitions.key?('ports')
             { 'HostConfig' => { 'PortBindings' => Hash[definitions['ports'].map do |ports|
               ["#{ports.split(':')[0]}/tcp", [{ 'HostPort' => ports.split(':')[1] }]]
@@ -71,19 +85,26 @@ module DockerizeRails
           else
             {}
           end
-        ).merge(
+        )
+        options = Helpers.recurse_merge(
+          options,
           definitions.key?('environment') ? { 'Env' => definitions['environment'] } : {}
-        ).merge(
+        )
+        options = Helpers.recurse_merge(
+          options,
           if definitions.key?('links')
-            { 'Links' => definitions['links'].map do |link|
+            { 'HostConfig' => { 'Links' => definitions['links'].map do |link|
               puts link, link.split(':')[1].to_sym
-              "#{link.split(':')[1]}:#{Helpers.get_name(link.split(':')[0].to_sym, :container)}"
-            end }
+              "#{Helpers.get_name(link.split(':')[0].to_sym, :container)}:#{link.split(':')[1]}"
+            end } }
           else
             {}
           end
         )
+        options
       end
+      # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
--- lib/dockerize_rails/docker_commands/helpers.rb
+++ lib/dockerize_rails/docker_commands/helpers.rb
@@ -3,14 +3,14 @@ module DockerizeRails
     module Helpers
       NAMES = {
         image: {
-            rails: "#{DRConfig.application_name}_rails:#{DRConfig.application_env}".freeze,
-            mysql: "#{DRConfig.application_name}_mysql:#{DRConfig.application_env}".freeze,
-            postgres: "#{DRConfig.application_name}_postgres:#{DRConfig.application_env}".freeze,
+          rails: "#{DRConfig.application_name}_rails:#{DRConfig.application_env}".freeze,
+          mysql: "#{DRConfig.application_name}_mysql:#{DRConfig.application_env}".freeze,
+          postgres: "#{DRConfig.application_name}_postgres:#{DRConfig.application_env}".freeze
         }.freeze,
         container: {
-            rails: "#{DRConfig.application_name}_rails_container".freeze,
-            mysql: "#{DRConfig.application_name}_mysql_container".freeze,
-            postgres: "#{DRConfig.application_name}_postgres_container".freeze,
+          rails: "#{DRConfig.application_name}_rails_container".freeze,
+          mysql: "#{DRConfig.application_name}_mysql_container".freeze,
+          postgres: "#{DRConfig.application_name}_postgres_container".freeze
         }.freeze
       }.freeze
 
@@ -21,6 +21,12 @@ module DockerizeRails
       def self.services_from_docker_compose
         docker_compose = YAML.load_file "#{DockerizeRails::PATHS.current}/docker-compose.yml"
         docker_compose['services']
+      end
+
+      def self.recurse_merge(a, b)
+        a.merge(b) do |_, x, y|
+          x.is_a?(Hash) && y.is_a?(Hash) ? recurse_merge(x, y) : [*x, *y]
+        end
       end
     end
   end
